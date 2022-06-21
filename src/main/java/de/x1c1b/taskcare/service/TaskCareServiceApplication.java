6f57aacdf6112187b041deb0f44fdeed75cf682c
Constantin Müller <32311121+0x1C1B@users.noreply.github.com>
2022-06-21 10:02:01 +0200

    build: initial commit
    Set up a Spring based project and configure all required dependencies.

---
+++ src/main/java/de/x1c1b/taskcare/service/TaskCareServiceApplication.java
@@ -0,0 +1,12 @@
+package de.x1c1b.taskcare.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskCareServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskCareServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/de/x1c1b/taskcare/service/TaskCareServiceApplicationTest.java
@@ -0,0 +1,14 @@
+package de.x1c1b.taskcare.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class TaskCareServiceApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+}
